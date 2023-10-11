442b83ddb9ccd8eb7573e003c1a3ed8c59021463
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-10-11 21:20:44 +0200

    pipelinetest

--- websec-react/src/App.tsx
+++ websec-react/src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Test! If this showing the pipeline works! :D
       </p>
     </>
   )

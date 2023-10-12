edf25d63760fd4732d4bef6f18c2622da4cd55c7
Yulia Gutorova <j.gutorova@gmail.com>
2023-10-12 19:24:30 +0200

    Revert "styla registrering och login form (registrering) #34"
    This reverts commit 500a8abea426aaa4c8031ccc7aa820b73cd09156.
    revert

--- websec-react/src/view/registration/RegistrationView.tsx
+++ websec-react/src/view/registration/RegistrationView.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import axios from "axios"
-
-import classes from "./styles/RegistrationView.module.css";
-
 export const RegistrationView = () => {
 
   const [username, setUsername] = useState("");
@@ -13,81 +10,62 @@ export const RegistrationView = () => {
     e.preventDefault();
 
     const user = {
-      username: username,
-      password: password
+      username :  username,
+      password : password
     }
 
     try {
 
       const resp = await axios.post(import.meta.env.VITE_REG_URL, user);
       console.log("Responce: " + resp.data);
-
+        
     } catch (error) {
-      console.log(error)
+        console.log(error)
     }
 
-
+    
     /* navigate("/mypage") */
     console.log("Username: " + username);
-    console.log("Password: " + password);
+    console.log("Password: " + password);  
   }
-  //================================================  
+//================================================  
   return (
-
-    <body>
-
-      <div className={classes.registerArea}>
-
-        <div className={classes.background}>
-          <div className={classes.shapeOne}></div>
-          <div className={classes.shapeTwo}></div>
+    <>
+      <h1>Register</h1>
+      <form >
+        
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
         </div>
 
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
 
-        <form className={classes.registerForm}>
-
-          <div >
-            <h1 className={classes.registerHeader}>Register</h1>
-          </div>
-
-          <div>
-            <label htmlFor="username" className={classes.registerLabel}>Username:</label>
-            <input
-              id="username"
-              type="text"
-              className={classes.registerInput}
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password" className={classes.registerLabel}>Password:</label>
-            <input
-              id="password"
-              type="password"
-              className={classes.registerInput}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-
-          <div>
-            <button type="button" onClick={handleSubmit} className={classes.registerButton}>Submit</button>
-          </div>
-
-          <hr className={classes.hr} />
-
-          <div>
-            <span>Already have an account?</span>
-            <Link to="/" className={classes.link}> <span className={classes.backLink}> {"< "}Go back </span> </Link>
-          </div>
-
-        </form>
-      </div>
-    </body>
-
+        <div>
+          <button type="button" onClick={handleSubmit}>Submit</button>
+        </div>
+        <div>
+          <span>Already have an account?</span>
+          <Link to="/"> Go back </Link>
+        </div>
+        
+      </form>
+    </>
+    
   );
 };

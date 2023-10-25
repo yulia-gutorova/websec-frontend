e1c659e45f09eba0be3ab73413f6d2b33e0a5baa
Yulia Gutorova <j.gutorova@gmail.com>
2023-10-25 17:56:44 +0200

    skapa MyPage #73

--- websec-react/src/view/myPage/MyPage.tsx
+++ websec-react/src/view/myPage/MyPage.tsx
@@ -1,11 +1,12 @@
-import {useEffect} from'react'
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import classes from "./styles/MyPage.module.css";
 
 export const MyPage = () => {
   const navigate = useNavigate()
-  const {name} = useParams()
-  const checkIfAuthed  = async () => {
+  const { name } = useParams()
+  const checkIfAuthed = async () => {
     try {
       const resp = await fetch(import.meta.env.VITE_BASE_URL + "/check-session", {
         method: "GET",
@@ -14,26 +15,31 @@ export const MyPage = () => {
         },
         credentials: "include",
       });
-  
+
       if (resp.status === 401) {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-       
+
         navigate('/login')
       }
-    } catch(error){
+    } catch (error) {
       navigate('/login')
       console.error(error)
-    } 
-  } 
-  
-  useEffect(() =>{
+    }
+  }
+
+  useEffect(() => {
     checkIfAuthed()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
 
   return (
-    <div>{name}</div>
+    <div className={classes.container}>
+    <div>
+        <h1 className={classes.myPageHeader}>Hello</h1>
+        <p className={classes.mypageInfo}>This is your page, {name}</p>
+    </div>
+    <div className={classes.boll}></div>
+</div>
   )
 }

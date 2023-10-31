78901328fd138087ded3ee2ee0af3da787c9345d
Yulia Gutorova <j.gutorova@gmail.com>
2023-10-31 21:37:44 +0100

    Style:a register + login cookie consent (register) #92

--- websec-react/src/view/registration/RegistrationView.tsx
+++ websec-react/src/view/registration/RegistrationView.tsx
@@ -14,7 +14,7 @@ interface IFormInput {
 
 export const RegistrationView = () => {
 
-  const {toggleBanner} = useConsent();
+  const { toggleBanner } = useConsent();
 
 
   const [errorMessage, setErrorMessage] = useState("")
@@ -32,7 +32,7 @@ export const RegistrationView = () => {
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const exportCookiesFromLocalStorage = () => {
-     
+
     try {
       const accessTokenObj = JSON.parse(localStorage.getItem("react-hook-consent") || '{}');
       const cookieBoolean = accessTokenObj["consent"][0] === "essentials"
@@ -41,17 +41,17 @@ export const RegistrationView = () => {
     } catch (error) {
       setCookieConsent(false)
     }
-   }  
- 
-useEffect(() => {
-  exportCookiesFromLocalStorage();
-}, [exportCookiesFromLocalStorage]);
+  }
+
+  useEffect(() => {
+    exportCookiesFromLocalStorage();
+  }, [exportCookiesFromLocalStorage]);
 
 
   const onSubmit: SubmitHandler<IFormInput> = async (data) => {
 
     if (data.checkbox) {
-      setIsLoading(true); 
+      setIsLoading(true);
       const user = {
         username: data.username,
         password: data.password,
@@ -92,7 +92,7 @@ useEffect(() => {
       } catch (error) {
         setErrorMessage("Unexpected error has occured")
       } finally {
-    setIsLoading(false);
+        setIsLoading(false);
       }
     }
 
@@ -100,102 +100,111 @@ useEffect(() => {
   //================================================
   return (
     <div className={classes.container}>
-        <Helmet>
-      <title>Register</title>
-      <meta name='Register' content='Secured'></meta>
+      <Helmet>
+        <title>Register</title>
+        <meta name='Register' content='Secured'></meta>
       </Helmet>
       <div className={classes.registerArea}>
+
         <div className={classes.background}>
           <div className={classes.shapeOne}></div>
           <div className={classes.shapeTwo}></div>
         </div>
 
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          id="registerForm"
-          className={classes.registerForm}>
+        <div className={classes.formWrapper}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            id="registerForm"
+            className={classes.registerForm}>
 
-          <div>
-            <h1
-              id="registerFormHeader" className={classes.registerHeader}>Register</h1>
-          </div>
-
-          {/* ------------- Username -----------------*/}
-          <div>
-            <label htmlFor="username" className={classes.registerLabel}>
-              Username:
-            </label>
-            <input
-              id="username"
-              type="text"
-              className={classes.registerInput}
-              {...register("username", { required: true })}
-            />
-
-            <div className={classes.registerErrorWrapper}>
-              {errors.username && <span className={classes.registerErrorText} id="usernameError" >Username is required</span>}
-            </div>
-          </div>
-
-          {/* ------------- Password -----------------*/}
-          <div>
-            <label htmlFor="password" className={classes.registerLabel}>
-              Password:
-            </label>
-            <input
-              id="password"
-              type="password"
-              className={classes.registerInput}
-              {...register("password", { required: true })}
-            />
-
-            <div className={classes.registerErrorWrapper}>
-              {errors.password && <span className={classes.registerErrorText} id="passwordError">Password is required</span>}
+            <div>
+              <h1
+                id="registerFormHeader" className={classes.registerHeader}>Register</h1>
             </div>
 
-          </div>
+            {/* ------------- Username -----------------*/}
+            <div>
+              <label htmlFor="username" className={classes.registerLabel}>
+                Username:
+              </label>
+              <input
+                id="username"
+                type="text"
+                className={classes.registerInput}
+                {...register("username", { required: true })}
+              />
 
-          {/* ------------- Checkbox -----------------*/}
-          <div className={classes.registerCheckboxWrapper}>
-            <input
-              id="checkbox"
-              type="checkbox"
-              onClick={() => setIsCheckbox(!isCheckbox)}
-              className={classes.registerInputCheckbox}
-              {...register("checkbox", { required: true })}
-            />
-            <label htmlFor="checkbox" className={classes.registerCheckboxLabel}>
-             <Link to="/policy">I agree to the Privacy Policy</Link>
-            </label>
+              <div className={classes.registerErrorWrapper}>
+                {errors.username && <span className={classes.registerErrorText} id="usernameError" >Username is required</span>}
+              </div>
+            </div>
+
+            {/* ------------- Password -----------------*/}
+            <div>
+              <label htmlFor="password" className={classes.registerLabel}>
+                Password:
+              </label>
+              <input
+                id="password"
+                type="password"
+                className={classes.registerInput}
+                {...register("password", { required: true })}
+              />
+
+              <div className={classes.registerErrorWrapper}>
+                {errors.password && <span className={classes.registerErrorText} id="passwordError">Password is required</span>}
+              </div>
+
+            </div>
+
+            {/* ------------- Checkbox -----------------*/}
+            <div className={classes.registerCheckboxWrapper}>
+              <input
+                id="checkbox"
+                type="checkbox"
+                onClick={() => setIsCheckbox(!isCheckbox)}
+                className={classes.registerInputCheckbox}
+                {...register("checkbox", { required: true })}
+              />
+              <label htmlFor="checkbox" className={classes.registerCheckboxLabel}>
+                <Link to="/policy">I agree to the Privacy Policy</Link>
+              </label>
+
+              <div className={classes.registerErrorWrapper}>
+                {errors.checkbox && <span className={classes.registerErrorText} id="checkboxError" >You must to agree to terms and conditions </span>}
+              </div>
+            </div>
+
+            <div>
+              <button disabled={isLoading || !cookieConsent} type="submit" className={`${classes.registerButton} ${isLoading || !cookieConsent ? classes.noHoverEffect : ""}`}>{isLoading ? 'Submitting...' : 'Submit'}</button>
+            </div>
 
             <div className={classes.registerErrorWrapper}>
-              {errors.checkbox && <span className={classes.registerErrorText} id="checkboxError" >You must to agree to terms and conditions </span>}
+
+              {errorMessage || !isCheckbox ?
+                (<span className={!isSubmitSuccessful ? classes.registerSuccessText : classes.registerErrorText} id="unexpectedError" >{errorMessage}</span>)
+                : (isSubmitSuccessful ? (<span className={classes.registerSuccessText}>Register successfull</span>) : null)}
             </div>
+
+
+            <hr className={classes.hr} />
+
+            <div>
+              <span>Already have an account?</span>
+              <Link to="/login" className={classes.link}><span className={classes.backLink}> &ensp; &ensp;{"< "}Go back</span></Link>
+            </div>
+
+          </form>
+
+          <div className={classes.registerCookieConsentWrapper}>
+            <p onClick={() => toggleBanner()} className={classes.registerCookieConsentLink}>I want to consent to cookies</p>
+            {!cookieConsent &&  <p className={classes.registerCookieConsentError}>Consent for essential cookies is required</p>}
           </div>
 
-          <div>
-            <button disabled={isLoading || !cookieConsent} type="submit" className={`${classes.registerButton} ${isLoading || !cookieConsent ? classes.noHoverEffect : ""}`}>{isLoading ? 'Submitting...' : 'Submit'}</button>
-          </div>
+         </div> {/* formWrapper */}
 
-          <div className={classes.registerErrorWrapper}>
-
-           {errorMessage || !isCheckbox ?
-                (<span className={!isSubmitSuccessful ? classes.registerSuccessText : classes.registerErrorText} id="unexpectedError" >{errorMessage}</span>) 
-                : (isSubmitSuccessful ? (<span className={classes.registerSuccessText}>Register successfull</span>) : null)}  
-          </div>
-
-
-          <hr className={classes.hr} />
-
-          <div>
-            <span>Already have an account?</span>
-            <Link to="/login" className={classes.link}><span className={classes.backLink}> &ensp; &ensp;{"< "}Go back</span></Link>
-          </div>
-
-        </form>
       </div>
-      <button onClick={() => toggleBanner()} className={classes.registerButton}>I want to consent to cookies</button>
-            {!cookieConsent && <div>"Consent for essential cookies is required to register"</div>}
+
     </div>
   );
 };
